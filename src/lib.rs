7a3de21277d8f98feb0bad6a1d80ffb1246cc96c
softprops <d.tangren@gmail.com>
2015-12-18 14:26:54 +0900

    impl basic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,85 @@
 extern crate hyper;
 extern crate time;
+extern crate rustc_serialize;
 
+use std::collections::HashMap;
 use std::sync::mpsc::Sender;
 use std::time::Duration;
 use std::thread::sleep;
 use hyper::Client;
 use hyper::status::StatusCode;
+
+#[derive(Debug, RustcDecodable)]
+pub struct WatchConfig {
+    url: String,
+    name: Option<String>,
+    interval: Option<u64>,
+    timeout: Option<usize>,
+    threshold: Option<usize>
+}
+
+#[derive(Debug, RustcDecodable)]
+pub struct NotifierConfig {
+    name: String,
+    config: Option<HashMap<String, String>>
+}
+
+#[derive(Debug, RustcDecodable)]
+pub struct Config {
+    watches: Vec<WatchConfig>,
+    notifiers: Vec<NotifierConfig>
+}
+
+impl Config {
+    pub fn watches(&self) -> Vec<Watch> {
+        self.watches.iter().map(|m| {
+            Watch::new(
+                m.name.clone().unwrap_or(m.url.clone()),
+                m.url.clone(),
+                Duration::from_millis(m.interval.unwrap_or(10_000)),
+                Duration::from_millis(m.interval.unwrap_or(5_000)),
+                m.threshold.unwrap_or(1)
+            )
+        }).collect::<Vec<Watch>>()
+    }
+
+    pub fn notifiers(&self) -> Vec<Box<Notifier>> {
+        self.notifiers.iter().filter_map(|n| {
+            Notifier::new(
+                n.name.clone(),
+                n.config.clone().unwrap_or(HashMap::new()))
+        }).collect::<Vec<Box<Notifier>>>()
+    }
+}
+
+pub trait Notifier {
+    fn notify(&self, watch: &Watch, log: &Log) -> ();
+    fn close(&self) -> ();
+}
+
+struct Email;
+
+impl Notifier for Email {
+    fn notify(&self, watch: &Watch, log: &Log) {}
+    fn close(&self) {}
+}
+
+struct PagerDuty;
+
+impl Notifier for PagerDuty {
+    fn notify(&self, watch: &Watch, log: &Log) {}
+    fn close(&self) {}
+}
+
+impl Notifier {
+    pub fn new<S>(name: S, config: HashMap<String, String>) -> Option<Box<Notifier>> where S: Into<String> {
+        match name.into().as_ref() {
+            "email" => Some(Box::new(Email)),
+            "pagerduty" => Some(Box::new(PagerDuty)),
+            _ => None
+        }
+    }
+}
 
 pub struct Log {
     entries: Vec<Entry>
@@ -28,20 +102,31 @@ pub struct Entry {
     pub result: Result
 }
 
-pub struct Monitor<'a> {
-    pub name: &'a str,
-    pub url: &'a str,
+#[derive(Debug)]
+pub struct Watch {
+    pub name: String,
+    pub url: String,
     pub interval: Duration,
-    pub timeout: Duration
+    pub timeout: Duration,
+    pub threshold: usize
 }
 
-impl <'a> Monitor <'a> {
+impl Watch  {
+    pub fn new<N, U>(name: N, url: U, interval: Duration, timeout: Duration, threshold: usize) -> Watch where N: Into<String>, U: Into<String> {
+        Watch {
+            name: name.into(),
+            url: url.into(),
+            interval: interval,
+            timeout: timeout,
+            threshold: threshold
+        }
+    }
     pub fn watch(&self, tx: Sender<Entry>) {
         let mut client = Client::new();
         client.set_read_timeout(Some(self.timeout));
         loop {
             let moment = time::now();
-            let result = client.head(self.url).send();
+            let result = client.head(&self.url[..]).send();
             let elapsed = time::now() - moment;
             match result {
                 Ok(response) => {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 extern crate coastguard;
+extern crate rustc_serialize;
 
-use std::time::Duration;
-use std::sync::mpsc::channel;
+use rustc_serialize::json;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let monitor = coastguard::Monitor {
-        name: "meetup",
-        url: "http://www.meetup.com/",
-        interval: Duration::from_secs(5),
-        timeout: Duration::from_secs(1)
-    };
-    let (tx, _) = channel();
-    monitor.watch(tx);
+    let mut f = File::open("coastguard.json").unwrap();
+    let mut s = String::new();
+    let _ = f.read_to_string(&mut s);
+    let config = json::decode::<coastguard::Config>(&s).unwrap();
+    println!("{:?}", config.watches())
 }
